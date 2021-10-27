c438a9e8a90edc066abce3159d81c8a8de9dd56d
murakami2135 <mr3.213333@gmail.com>
2021-10-27 13:13:18 +0000

    new group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,7 @@
 class Group < ApplicationRecord
+  has_many :group_users
+  #                ↓ここ何をやってるの？
+  has_many :users, through: :group_users
+  # ↓ここ何をやってるの？
+  validates :name, presence: true, uniqueness: true
 end
